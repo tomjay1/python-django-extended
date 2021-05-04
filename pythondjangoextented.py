c0bb18e46644c89337d0b13e826dc5119ad99f6e
OKPETE TOMJAY <81090931+tomjay1@users.noreply.github.com>
2021-05-04 23:58:04 +0100

    Add files via upload

---
+++ pythondjangoextented.py
@@ -0,0 +1,11 @@
+pythondjangoextented
+#register
+#-first name, last name, password, email
+#-generate useraccount
+
+#login
+#-account number and password
+
+#bank operation
+
+#initialization process
